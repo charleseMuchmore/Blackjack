0801ecd52a666a7cddd545abb9ec9d2d93828dd3
charleseMuchmore <87589934+charleseMuchmore@users.noreply.github.com>
2021-10-20 20:02:01 -0700

    No longer can see all of compy cards

--- A main.py
+++ A main.py
@@ -4,10 +4,8 @@ from random import *
 
 play_game = input("Do you want to play a game of Blackjack? type 'y' or 'n': ")
 deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-if play_game == 'n' or play_game == 'no':
-    clear()
 #dealing the initial cards
-elif play_game == 'y' or play_game == 'yes':
+if play_game == 'y' or play_game == 'yes':
     clear()
     print (logo)
     user_cards = []
@@ -16,38 +14,39 @@ elif play_game == 'y' or play_game == 'yes':
     print(f"    Your cards: {user_cards}, current score: {sumUser_cards}")
     compy_cards = []
     compy_cards = deal_cards(deck, compy_cards, 2)
-    # print(f"    Computer's first card: {compy_cards[0]}")
-    print(f"    Computer's cards: {compy_cards}")
+    print(f"    Computer's first card: {compy_cards[0]}")
 
-#cont dealing user cards
-more_card = input("Type 'y' to get another card, type 'n' to pass: ")
+    #cont dealing cards
+    more_card = input("Type 'y' to get another card, type 'n' to pass: ")
 
-sumCompy_cards = card_add(compy_cards)
-if more_card == 'n' or sumUser_cards >= 21:
-    determine_winner(sumUser_cards, sumCompy_cards)
+    sumCompy_cards = card_add(compy_cards)
+    if more_card == 'n' or sumUser_cards >= 21:
+        determine_winner(sumUser_cards, sumCompy_cards)
 
-print(' ')
-while more_card == 'y':
-    if sumUser_cards <= 21:
-        deal_cards(deck, user_cards, 1)
-        sumUser_cards = sumUser_cards + user_cards[-1]
-        compy_cards = cardAdd4Compy(compy_cards, deck)
-        print(f"    Your cards: {user_cards}, current score: {sumUser_cards}")
-        # print(f"    Computer's first card: {compy_cards[0]}")
-        print(f"    Computer's cards: {compy_cards}")
-        if sumUser_cards <= 21:
-            more_card = input("Type 'y' to get another card, type 'n' to pass: ")
-        else:
-            more_card = 'n'
-        
-# print("While Loop has been exited")
-sumCompy_cards = card_add(compy_cards)
-if more_card == 'n':
-    win = determine_winner(sumUser_cards, sumCompy_cards)
     print(' ')
-    if win == 'Y':
-        print(f"Your final score: {sumUser_cards} \n The Computer's score: {sumCompy_cards} \n You Won!!")
-    elif win == 'N':
-        print(f"Your final score: {sumUser_cards} \n The Computer's score: {sumCompy_cards} \n You Lost!!")
-    elif win == 'D':
-        print(f"Your final score: {sumUser_cards} \n The Computer's score: {sumCompy_cards} \n It's a Draw!!")
+    while more_card == 'y':
+        if sumUser_cards <= 21:
+            deal_cards(deck, user_cards, 1)
+            sumUser_cards = sumUser_cards + user_cards[-1]
+            compy_cards = cardAdd4Compy(compy_cards, deck)
+            print(f"    Your cards: {user_cards}, current score: {sumUser_cards}")
+            print(f"    Computer's first card: {compy_cards[0]}")
+            if sumUser_cards <= 21:
+                more_card = input("Type 'y' to get another card, type 'n' to pass: ")
+            else:
+                more_card = 'n'
+            
+
+    sumCompy_cards = card_add(compy_cards)
+    if more_card == 'n':
+        win = determine_winner(sumUser_cards, sumCompy_cards)
+        print(' ')
+        if win == 'Y':
+            print(f"Your final score: {sumUser_cards} \n The Computer's score: {sumCompy_cards} \n You Won!!")
+        elif win == 'N':
+            print(f"Your final score: {sumUser_cards} \n The Computer's score: {sumCompy_cards} \n You Lost!!")
+        elif win == 'D':
+            print(f"Your final score: {sumUser_cards} \n The Computer's score: {sumCompy_cards} \n It's a Draw!!")
+
+if play_game == 'n' or play_game == 'no':
+    clear()
